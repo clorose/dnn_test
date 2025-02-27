835f192cb1c39ec08083679348bdae9dc3367f3b
clorose <gohan9511@gmail.com>
2025-02-27 14:45:19 +0900

    🎨 Style: Change dashboard template background from red to gray

--- frontend/src/pages/dashboardPage/dashboardTemplate.tsx
+++ frontend/src/pages/dashboardPage/dashboardTemplate.tsx
@@ -1,3 +1,4 @@
+// path: frontend/src/pages/dashboardPage/dashboardTemplate.tsx
 import { Outlet } from "react-router-dom";
 import Header from "../../components/common/Header/Header";
 import Footer from "../../components/common/Footer/Footer";
@@ -6,7 +7,7 @@ const dashboardTemplate = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex-1 bg-red-50 mt-8">
+      <div className="flex-1 bg-gray-50 mt-12"> {/* 빨간 배경색 대신 더 중립적인 회색 배경으로 변경 */}
         <Outlet />
       </div>
       <Footer />
